88cabca04bcea6ba60796213539a036820c34da3
Nadir Kutluozen <nadirkutluozen.nk@gmail.com>
2025-08-04 14:18:51 -0400

    added vercel analytcs

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,6 +4,7 @@ import Footer from '../components/footer/Footer'
 import '../components/App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';  
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import { Analytics } from '@vercel/analytics/react';
 
 
 
@@ -14,6 +15,7 @@ export default function App({ Component, pageProps }) {
     <>
       <Header />
       <Component {...pageProps} />
+      <Analytics />
       <Footer />
     </>
   )
