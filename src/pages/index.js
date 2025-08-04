fe29a3a0097a11f169fdc06e1048ff378787d677
Nadir Kutluozen <nadirkutluozen.nk@gmail.com>
2025-08-04 14:23:35 -0400

    added google ver

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,7 @@ export default function HomePage() {
     <>
       <Head>
         <title>Compare Online Courses | OnlineCourseCompare</title>
+        <meta name="google-site-verification" content="deaurSH-7sQaiipreMsppVW27TxCijtPN2GhD7oBnqE" />
         <meta name="description" content="Find and compare the best online courses from Udemy, Coursera, edX and more." />
         <meta name="keywords" content="online course compare, udemy vs coursera, best online course 2025" />
         <meta name="author" content="Nadir Kutluozen" />
