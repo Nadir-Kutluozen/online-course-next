1ab68b3290f3f70c6c6309ba98138d5f426215a8
Nadir Kutluozen <nadirkutluozen.nk@gmail.com>
2025-08-04 14:42:22 -0400

    added favicon

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,7 +5,7 @@ import '../components/App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';  
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import { Analytics } from '@vercel/analytics/react';
-
+import Head from 'next/head';
 
 
 
@@ -13,6 +13,11 @@ import { Analytics } from '@vercel/analytics/react';
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <link rel="icon" href="./logo.svg" />
+        
+        <title>Online Course Compare</title>
+      </Head>
       <Header />
       <Component {...pageProps} />
       <Analytics />
